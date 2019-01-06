170cdd03d000e15b3cca89d2187cdc0138db6c97
FMGordillo <facundomgordillo@gmail.com>
2019-01-06 14:39:39 -0300

    Layout update ✨

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>¡Felicitaciones! Tu aplicación funciona bien</p>
+					<a
+						className="App-link"
+						href="https://platzi.com/cursos/react/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Aprendé React.js
+					</a>
+					<a
+						className="App-link"
+						href="https://platzi.com/cursos/express-js/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Aprendé Express.js
+					</a>
+					<a
+						className="App-link"
+						href="https://developer.ibm.com/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Visitá IBM Developer
+					</a>
+				</header>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
